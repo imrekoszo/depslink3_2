1e669da5964547b1eb76fc804ee9c42059168426
Imre Kószó <imrekoszo@users.noreply.github.com>
2020-11-23 13:52:54 +0100

    Add basic code

---
+++ src/app/core.clj
@@ -0,0 +1,11 @@
+(ns app.core
+  (:gen-class)
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]))
+
+(defn -main [& args]
+  (println "Hello world, here's my deps.edn:"
+           (-> "project-deps.edn"
+               io/resource
+               slurp
+               edn/read-string)))
